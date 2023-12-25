200aef0747d82bcfa90dc63c10335cbb0631a7e5
jassyio <joejassyio@gmail.com>
2023-12-25 12:02:44 +0300

    file_storage

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,21 +7,22 @@ from models.city import City
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Teardown """
+def closedb(exc):
+    """ to close a database session"""
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def display_html():
-    """ Function called with /states_list route """
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html',
-                           Table="States",
-                           states=states)
+@app.route('/cities_by_states')
+def states_list():
+    """ /states_list route """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    storage.reload()
+    app.run("0.0.0.0", 5000)
