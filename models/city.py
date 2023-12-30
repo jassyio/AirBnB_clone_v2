46cbe390a3ddbb5e89aa9a586b5939890d26ad38
jassyio <joejassyio@gmail.com>
2023-12-30 08:30:01 +0300

    same commit

--- models/city.py
+++ models/city.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 """ holds class City"""
 import models
-# File: city.py
-
 from models.base_model import BaseModel, Base
 from os import getenv
-from sqlalchemy import Column, String, ForeignKey
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+
 
 class City(BaseModel, Base):
-    """Representation of city"""
-    __tablename__ = 'cities'
-
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities", cascade="all, delete-orphan")
-
+    """Representation of city """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(60),
+                      nullable=False)
+        state_id = Column(String(60),
+                          ForeignKey('states.id'),
+                          nullable=False)
+        places = relationship("Place",
+                              backref="cities",
+                              cascade="all, delete-orphan")
+    else:
+        name = ""
+        state_id = ""
 
     def __init__(self, *args, **kwargs):
         """initializes city"""
