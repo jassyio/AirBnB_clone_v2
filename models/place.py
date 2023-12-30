62a13c47c9461852f54f1b564aaae5077086f969
jassyio <joejassyio@gmail.com>
2023-12-30 08:20:05 +0300

    changed city and place

--- models/place.py
+++ models/place.py
@@ -1,72 +1,29 @@
 #!/usr/bin/python3
 """ holds class Place"""
 import models
+# File: place.py
+
 from models.base_model import BaseModel, Base
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, Integer, Float, Table, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id',
-                                 String(60),
-                                 ForeignKey('places.id'),
-                                 primary_key=True,
-                                 nullable=False),
-                          Column('amenity_id',
-                                 String(60),
-                                 ForeignKey('amenities.id'),
-                                 primary_key=True,
-                                 nullable=False))
-
-
 class Place(BaseModel, Base):
-    """Representation of Place """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'places'
-        city_id = Column(String(60),
-                         ForeignKey("cities.id"),
-                         nullable=False)
-        user_id = Column(String(60),
-                         ForeignKey('users.id'),
-                         nullable=False)
-        name = Column(String(128),
-                      nullable=False)
-        description = Column(String(1024),
-                             nullable=True)
-        number_rooms = Column(Integer,
-                              default=0,
-                              nullable=False)
-        number_bathrooms = Column(Integer,
-                                  default=0,
-                                  nullable=False)
-        max_guest = Column(Integer,
-                           default=0,
-                           nullable=False)
-        price_by_night = Column(Integer,
-                                default=0,
-                                nullable=False)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", cascade="all, delete",
-                               backref="places")
-        amenities = relationship("Amenity",
-                                 secondary='place_amenity',
-                                 viewonly=False,
-                                 backref="place_amenities")
-    else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
+    """Representation of Place"""
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    reviews = relationship("Review", cascade="all, delete", backref="places")
+    amenities = relationship("Amenity", secondary='place_amenity', viewonly=False, backref="place_amenities")
 
     def __init__(self, *args, **kwargs):
         """initializes Place"""
