aba1b0992313b5822a7ded5d7bf3cfa9ddc8db44
jassyio <joejassyio@gmail.com>
2024-01-01 15:06:01 +0300

    .\7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Importing Flask to run the web app"""
 from flask import Flask, render_template
-from models import storage
+# from models.engine.db_storage import DBStorage
 
 
 app = Flask(__name__)
@@ -10,8 +10,9 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def display_states():
     """Render state_list html page to display States created"""
-    states = storage.all()
-    return render_template('7-states_list.html', states=states)
+    states = storage.all("State").values()  # Assuming State is the model representing states
+    sorted_states = sorted(states, key=lambda state: state.name)  # Sort states by name
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
@@ -22,3 +23,4 @@ def teardown(self):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+ 
